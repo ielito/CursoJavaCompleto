bff186b1ea09502aa73bf45cfb9d9d4bfd1359e5
Rafa ielo <140087246+ielito@users.noreply.github.com>
2024-07-17 08:06:49 -0300

    First Commit

---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,32 @@
+package fundamentos;
+
+
+	/**
+	 * Essa classe representa... Comentário JAVADOC
+	 * 
+	 * @author Rafael Ielo <rafael@ielo.org>
+	 * @since JDK1.0
+	 * 
+	 */
+
+public class PrimeiroPrograma {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		
+		// Uma sentença de código termina com ;
+		// Mais um comentário
+		System.out.println("Olá Mundão triste sem Low Code - Part #01!!!");
+		/*
+		 * Várias Linhas de código usando /*
+		 * CMD / comenta/descomenta - atalho
+		 */
+		System.out.println("Olá Mundão triste sem Low Code - Part #02!!!");
+
+	}
+
+}
